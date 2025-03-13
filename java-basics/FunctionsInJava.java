e10bb2a9833bfb8089ac286404edb60f4555f245
Antara Chanda <160458895+meantaraa@users.noreply.github.com>
2025-03-13 08:28:14 +0530

    FunctionsInJava.java

--- java-basics/FunctionsInJava.java
+++ java-basics/FunctionsInJava.java
@@ -1,10 +1,25 @@
-public class FunctionsInJava {
-    public static int add(int a, int b) {
+import java.util.Scanner;
+
+public class Functions {
+    public static int addition(int a, int b){
         return a + b;
     }
 
     public static void main(String[] args) {
-        int sum = add(10, 20);
-        System.out.println("Sum: " + sum);
-    }  
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+
+        int result = addition(num1, num2);
+        
+        System.out.println("The result of addition is: " + result);
+
+        scanner.close();
+    }
 }
+
